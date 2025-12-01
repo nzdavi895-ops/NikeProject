3514dc8fbde845bae0e37831a6b3b49b3b8a78b5
nzdavi895-ops <nzdavi895@gmail.com>
2025-12-01 20:57:03 -0300

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,7 @@
+// Se não houver modo escuro, o botão não precisa fazer nada
+const btn = document.getElementById("themeToggle");
+
+// Opcional: desativar o botão
+if (btn) {
+  btn.style.display = "none"; // esconde o botão
+}
